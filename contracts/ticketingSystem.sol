b422d4333626b6a0511ea0ab9b570d9836de9ce1
Thibault <thibault.rieul@gmail.com>
2020-03-27 23:02:36 +0100

    emit ticket

--- contracts/ticketingSystem.sol
+++ contracts/ticketingSystem.sol
@@ -5,7 +5,7 @@ contract ticketingSystem {
     struct Artist{
         bytes32 name;
         uint artistCategory;
-        address owner;
+        address payable owner;
         uint ticketSold;
     }
 
@@ -13,7 +13,7 @@ contract ticketingSystem {
         bytes32 name;
         uint capacity;
         uint standardComission;
-        address owner;
+        address payable owner;
     }
 
     struct Concert{
@@ -21,25 +21,26 @@ contract ticketingSystem {
         uint venueId;
         uint concertDate;
         uint ticketPrice;
-        uint totalsldTicket;
+        uint totalsoldTicket;
         uint totalMoneyCollected;
         bool validatedByArtist;
         bool validatedByVenue;
-        address owner;
+        address payable owner;
         
     }
 
     struct Ticket{
         uint concertId;
-        address owner;
+        address payable owner;
         uint amountPaid;
         bool isAvailable;
         bool idAvailableForSale;
     }
 
-    event NewArtist(bytes32 _name, uint _artistCategory,address _owner,uint _ticketSold);
-    event NewVenue(bytes32 _name, uint _capacity, uint _standardComission,address _owner);
-    event NewConcert(uint _artistId, uint _venueId, uint _concertDate, uint _ticketPrice, address _owner);
+    event NewArtist(bytes32 _name, uint _artistCategory,address payable _owner,uint _ticketSold);
+    event NewVenue(bytes32 _name, uint _capacity, uint _standardComission,address payable _owner);
+    event NewConcert(uint _artistId, uint _venueId, uint _concertDate, uint _ticketPrice, address payable _owner);
+    event NewTicket(uint _concertId, address _ticketOwner);
 
     mapping(uint => Artist) L_Artist;
     mapping(uint => Venue) L_Venue;
@@ -57,14 +58,14 @@ contract ticketingSystem {
         Artist memory a = Artist(_name,_artistCategory,msg.sender,0);
         L_Artist[artist_count] = a;
         artist_count = artist_count + 1;
-        //emit NewArtist(_name,_artistCategory,msg.sender,0);
+        emit NewArtist(_name,_artistCategory,msg.sender,0);
     }
 
     function artistsRegister(uint id) public view returns(Artist memory) {
         return(L_Artist[id]);
     }
 
-    function modifyArtist(uint _artistId, bytes32 _name, uint _artistCategory, address _newOwner) 
+    function modifyArtist(uint _artistId, bytes32 _name, uint _artistCategory, address payable _newOwner) 
     public OnlyOwnerArtist(_artistId){
         L_Artist[_artistId].name = _name;
         L_Artist[_artistId].artistCategory = _artistCategory;
@@ -85,14 +86,14 @@ contract ticketingSystem {
         Venue memory v = Venue(_name,_capacity,_standardComission,msg.sender);
         L_Venue[venue_count] = v;
         venue_count = venue_count + 1;
-        //emit NewVenue(_name,_capacity,_standardComission,msg.sender);
+        emit NewVenue(_name,_capacity,_standardComission,msg.sender);
     }
 
     function venueRegister(uint id) public view returns(Venue memory) {
         return(L_Venue[id]);
     }
 
-    function modifyVenue(uint _venueId, bytes32 _name, uint _capacity, uint _standardComission, address _newOwner) public OnlyOwnerVenue(_venueId) {
+    function modifyVenue(uint _venueId, bytes32 _name, uint _capacity, uint _standardComission, address payable _newOwner) public OnlyOwnerVenue(_venueId) {
        L_Venue[_venueId].name=_name;
        L_Venue[_venueId].capacity=_capacity;
        L_Venue[_venueId].standardComission=_standardComission;
@@ -106,7 +107,27 @@ contract ticketingSystem {
         Concert memory c = Concert(_artistId,_venueId,_concertDate,_ticketPrice,0,0,_validatedByArtist,_validatedByVenue,msg.sender);
         L_Concert[concert_count] = c;
         concert_count = concert_count + 1;
-        //emit NewConcert(_artistId,_venueId,_concertDate,_ticketPrice,msg.sender);
+        emit NewConcert(_artistId,_venueId,_concertDate,_ticketPrice,msg.sender);
     }
+
+    function concertRegister(uint id) public view returns(Concert memory) {
+        return(L_Concert[id]);
+    }
+
+    function validateConcert(uint _concertId) public {
+        L_Concert[_concertId].validatedByArtist = msg.sender == L_Artist[L_Concert[_concertId].artistId].owner;
+        L_Concert[_concertId].validatedByVenue = msg.sender == L_Venue[L_Concert[_concertId].venueId].owner;  
+    }
+
+    function emitTicket(uint _concertId, address payable _ticketOwner) public {
+        require(msg.sender == L_Artist[L_Concert[_concertId].artistId].owner, "You have to buy the ticket");
+        Ticket memory t = Ticket(_concertId, _ticketOwner, true, false, L_Concert[_concertId].ticketPrice);
+        L_Ticket[ticket_count] = t;
+        ticket_count = ticket_count + 1;
+        L_Concert[_concertId].totalsoldTicket = L_Concert[_concertId].totalsoldTicket + 1;
+        emit NewTicket(_concertId, _ticketOwner);
+    }
+
+
 
 }
